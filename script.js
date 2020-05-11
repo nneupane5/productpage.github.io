9887df3802b3b58f08a989f0b8daf41abc6f6110
nneupane5 <47231960+nneupane5@users.noreply.github.com>
2020-05-10 20:17:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function searchproduct() {
+    let input= document.getElementById('productlist').value
+    input= input.toLowerCase();
+    let x =document.getElementsByClassName('box');
+
+    for (i = 0; i < x.length; i++){
+        if (!x[i].innerHTML.toLowerCase().includes(input)){
+            x[i].style.display="none";
+        }
+        else{
+            x[i].style.display="list-item";
+        }
+    }
+}
